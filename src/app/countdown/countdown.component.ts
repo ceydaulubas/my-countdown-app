d28a382deb6f5b26f83f757d6496b70de609086d
Ceyda Ulubas Tanfener <ceydaulubas@Ceydas-MacBook-Air.local>
2024-02-11 22:03:56 +0300

    edit CountdownComponent and html file

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,26 +1,49 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { CountdownForm } from '../countdownForm';
 
 
 // date picker imports
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
+
+import { TimeService } from '../services/time.service'; // Import the service
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [ MatFormFieldModule, MatInputModule, MatIconModule, MatDatepickerModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatDatepickerModule, FormsModule, CommonModule, NgIf],
   styleUrl: './countdown.component.scss'
+
 })
 export class CountdownComponent {
+  constructor(private timeService: TimeService) { }
+
   hero: CountdownForm = {
-    title: 'Time to Midsummer  Eve 2022!',
-    date: new Date('2022-06-24')
+    title: '',
+    date: new Date()
   };
+
+  onTitleChange(newTitle: string) {
+    this.hero.title = newTitle;
+  }
+
+  onDateChange(newDate: Date) {
+    this.hero.date = newDate as Date;
+  }
+
+  calculateTimeDifference(targetDate: Date): string {
+    return this.timeService.getTimeDifference(targetDate);
+  }
 }
+
+
--- src/app/countdownForm.ts
+++ src/app/countdownForm.ts
@@ -1,4 +1,5 @@
 export interface CountdownForm {
     title: string;
-    date: Date;
+    date: Date ;
   }
+  
